6546f413ca78b98a3e59cb1463906190f97d9e8a
Peter Grman <peter.grman@gmail.com>
2015-01-08 18:11:52 +0100

    exclude compiled css files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
   sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./public/css/*.scss')
+  gulp.src('./public/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./public/css'))
     .pipe(livereload());
