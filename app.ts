d099e9d64f00acdd724c93d74fd3a3c7b51f84c3
Peter Grman <peter.grman@gmail.com>
2015-01-08 19:02:36 +0100

    Create sign-up form

--- app.ts
+++ app.ts
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
 
 var app = express();
 
@@ -29,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,11 @@ var router = express.Router();
 
 // GET home page.
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'VimFika' });
+})
+
+router.post('/signup', (req, res) => {
+  res.json(req.body);    
 })
 
 export = router;
