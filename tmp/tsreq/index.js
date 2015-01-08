f8ab0d7c3e502099ceb2efea008fbb8a17bf106a
Peter Grman <peter.grman@gmail.com>
2015-01-08 19:02:01 +0100

    Remove and ignore tmp files

--- tmp/tsreq/index.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-var express = require('express');
-var router = express.Router();
-// GET home page.
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
-module.exports = router;
--- tmp/tsreq/user.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-var express = require('express');
-var router = express.Router();
-// GET users listing.
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
-});
-module.exports = router;
